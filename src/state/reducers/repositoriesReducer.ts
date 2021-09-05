13459cc54832464488ddfc25363b6ea3a56810bf
builtbyjosh <pc.refuge@gmail.com>
2021-09-05 15:07:49 -0400

    app setup and reducer setup

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,20 @@
+interface RepositoriesState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+const reducer = (state: RepositoriesState, action: any) => {
+  switch (action.type) {
+    case 'search_respositories':
+      return { loading: true, error: null, data: []}
+    case 'search_respositories_success':
+      return { loading: false, error: null, data: action.payload}
+    case 'search_respositories_error':
+      return { loading: false, error: action.payload, data: []}
+    default:
+      return state
+  }
+}
+
+export default reducer
